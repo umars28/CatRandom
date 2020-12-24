f0bd020181f42c6837becd69994827db0bf18031
Umar Sabirin <umarsabirin369@gmail.com>
2020-12-24 23:57:41 +0800

    add some feature

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
           text-align: center;
           margin-top: 100px;
         }
+        .footer {
+              color: grey;
+          }
       </style>
     <meta name="viewport" content="width=device-width, minimum-scale=0.1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -22,5 +25,10 @@
             <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="{{ $getRandomCat }}" width="394" height="338">
             @endif
         </div>
+        <section class="footer" id="footer">
+            <footer class="navbar-nav bg-outline-primary text-center pt-4 pb-3 mt-4">
+              <h6>&#169; Copyright by Umar Sabirin</h6>
+            </footer>
+          </section>
     </body>
 </html>
